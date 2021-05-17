d31239a2d09a991f38cf998a97332bc2af59f3e5
Gua Hao Khov <khovg@student.ethz.ch>
2021-05-18 01:41:05 +0200

    [apps] Add parametrized sequential memory region with reserved region for queues

--- config/config.mk
+++ config/config.mk
@@ -33,6 +33,22 @@ l2_base  ?= 80000000
 l2_size  ?= 10000
 l2_banks ?= 4
 
+# Size of sequential memory per core (in bytes)
+# (must be a power of two)
+seq_mem_size ?= 2048
+
+# Size of stack in sequential memory per core (in bytes)
+# (must not be larger than the sequential memory - queue)
+stack_size ?= 1024
+
+#############################
+##  Xqueues configuration  ##
+#############################
+
+# Size of queue in each memory bank (in words)
+# (assume banking factor of 4)
+queue_size ?= 4
+
 ################################
 ##  Optional functionalities  ##
 ################################
